1703e5947d897c9ef968067fc03830a67c7be899
sercansimsek <sercansimsek44@gmail.com>
2025-10-24 13:55:35 +0200

    mobile design cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import qrCode from "./images/image-qr-code.png";
 
 export const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-slate-300 h-screen">
+    <div className="flex flex-col justify-center items-center bg-[#D5E1EF] h-screen">
       <div className="flex flex-col px-4 pt-4 pb-6 gap-6 rounded-xl bg-current h-[500px] w-[320px]">
         <img
           className="w-[288px] h-[288px] rounded-lg"
@@ -10,8 +10,8 @@ export const App = () => {
           alt="qr=code"
         />
         <div className="flex flex-col text-center gap-4">
-          <p className="text-slate-500 font-bold">Improve your front-end skills by building projects</p>
-          <p className="text-slate-900">
+          <p className="text-slate-900 font-bold text-[22px]">Improve your front-end skills by building projects</p>
+          <p className="text-slate-500 text-[15px] tracking-wide font-regular">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
