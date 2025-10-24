c5d054b7f8a228ae6d07f8fad854c460bbd3e05e
sercansimsek <sercansimsek44@gmail.com>
2025-10-24 13:41:49 +0200

    mobile desing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import qrCode from "./images/image-qr-code.png";
+
 export const App = () => {
-  return <div className="text-3xl">Hello, World!</div>;
-}
+  return (
+    <div className="flex flex-col justify-center items-center bg-slate-300 h-screen">
+      <div className="flex flex-col px-4 pt-4 pb-6 gap-6 rounded-xl bg-current h-[500px] w-[320px]">
+        <img
+          className="w-[288px] h-[288px] rounded-lg"
+          src={qrCode}
+          alt="qr=code"
+        />
+        <div className="flex flex-col text-center gap-4">
+          <p className="text-slate-500 font-bold">Improve your front-end skills by building projects</p>
+          <p className="text-slate-900">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
